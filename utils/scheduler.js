fe4c5aaae661e9e12ddaed748dafd29cc274b288
kidangelsam <61677770+kidangelsam@users.noreply.github.com>
2021-02-17 19:58:11 +0800

    update

--- utils/scheduler.js
+++ utils/scheduler.js
@@ -226,8 +226,8 @@ let scheduler = {
             }
 
             // 任务执行
-            let queue = new PQueue({ concurrency: 2 });
-            console.log('调度任务中', '并发数', 2)
+            let queue = new PQueue({ concurrency: 1 });
+            console.log('调度任务中', '并发数', 1)
             for (let task of will_tasks) {
                 queue.add(async () => {
                     try {
